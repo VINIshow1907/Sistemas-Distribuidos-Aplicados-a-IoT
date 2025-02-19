77d02340bae33adf771dcd8efe83946a78a968f8
VINIshow1907 <viniciuslucio792@gmail.com>
2025-02-19 20:30:47 -0300

    :sparkles: Aprendendo int em python

---
+++ 1semestre/integer.py
@@ -0,0 +1,5 @@
+numero = int (input("Digite um valor: "))
+print(type(numero))
+#print ("O numero ", numero, "foi digitado")
+#print ("O número {} foi digitado" .format(numero))
+print (f"O número {numero} foi digitado")
