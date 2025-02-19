b1201de833a288e5cf531b440da4c580f91b2ac7
VINIshow1907 <viniciuslucio792@gmail.com>
2025-02-19 20:58:47 -0300

    :sparkles: Operadores aritmeticos

---
+++ 1semestre/operadores aritméticos.py
@@ -0,0 +1,31 @@
+import math
+
+n1 = float(input("Digite um número:"))
+print()
+n2 = float(input("Digite outro número:"))
+
+r1 = n1 + n2
+print("Soma = ",r1)
+print()
+r2 = n1 - n2
+print("Subtração = ",r2)
+print()
+r3 = n1 * n2
+print("Multiplição = ",r3)
+print()
+r4 = n1 // n2
+print("Divisão inteira = ",r4)
+print()
+r5 = n1 / n2
+print("Divisão real = ",r5)
+print()
+r6 = n1 % n2
+print("Resto da divisão = ",r6)
+print()
+r7 = n1 ** n2
+print("Exponenciação = ",r7)
+print()
+r8 = math.sqrt(n2)
+print("Raiz Quadrada = ",r8)
+
+
