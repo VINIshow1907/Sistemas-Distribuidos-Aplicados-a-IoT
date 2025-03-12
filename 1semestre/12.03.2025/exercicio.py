eef820342cca177923d5a942347fe01c6efb2fd0
VINIshow1907 <viniciuslucio792@gmail.com>
2025-03-12 16:52:23 -0300

    :sparkles: Exercicio  número 25 até o 30

---
+++ 1semestre/12.03.2025/exercicio.py
@@ -0,0 +1,6 @@
+'Pegando o valor inicial e contando até o numero final'
+numero1 = int (input ("Digite o número inicial da contagem :"));
+numero2 = int (input ("Digite o número final da contagem : "));
+
+for contador in range(numero1,numero2 + 1):
+    print(contador)
