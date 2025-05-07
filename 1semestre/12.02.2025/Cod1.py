e73d8c47e6b303f4f81a645bd7a45c93fa53448f
VINIshow1907 <viniciuslucio792@gmail.com>
2025-05-07 16:54:56 -0300

    :recycle: Arrumando na pasta do dia 12.02.2025

---
+++ 1semestre/12.02.2025/Cod1.py
@@ -0,0 +1,1 @@
+print ("Palmeiras não tem mundial!!!!!!!!⌨")
---
+++ 1semestre/12.02.2025/float.py
@@ -0,0 +1,5 @@
+numero = float (input("Digite um valor: "))
+print(type(numero))
+#print ("O numero ", numero, "foi digitado")
+#print ("O número {} foi digitado" .format(numero))
+print (f"O número {numero} foi digitado")
